479773a540b165c39731a174a0a6fc1424e203f1
Xinyi <xinzwerkz@gmail.com>
2025-04-18 00:24:27 +0800

    fetch all combined data, and add country_name param to get data for specific country if desired.

--- create_ioda_timeseries.py
+++ create_ioda_timeseries.py
@@ -8,7 +8,7 @@ from constants import GCP_PROJECT_ID, NE_MAP_PATH
 LOOKBACK_PERIOD = 1  # in days
 
 
-def get_mozilla_data(country_name, end_time=datetime.datetime.now(), start_time=None):
+def get_mozilla_data(country_name=None, end_time=datetime.datetime.now(), start_time=None):
     if not isinstance(end_time, datetime.datetime):
         # if end_time is passed in as unix timestamp
         end_time = datetime.datetime.fromtimestamp(end_time)
@@ -23,13 +23,21 @@ def get_mozilla_data(country_name, end_time=datetime.datetime.now(), start_time=
     end_time_fmt = end_time.strftime("%Y-%m-%d %H:%M:%S")
     start_time_fmt = start_time.strftime("%Y-%m-%d %H:%M:%S")
 
-    query = f"""
-      SELECT *
-      FROM `moz-fx-data-shared-prod.internet_outages.global_outages_v2`
-      WHERE country = '{country_name}'
-      AND datetime BETWEEN TIMESTAMP('{start_time_fmt}')
-                       AND TIMESTAMP('{end_time_fmt}')
-      """
+    if country_name:
+        query = f"""
+          SELECT *
+          FROM `moz-fx-data-shared-prod.internet_outages.global_outages_v2`
+          WHERE country = '{country_name}'
+          AND datetime BETWEEN TIMESTAMP('{start_time_fmt}')
+                           AND TIMESTAMP('{end_time_fmt}')
+          """
+    else:
+        query = f"""
+          SELECT *
+          FROM `moz-fx-data-shared-prod.internet_outages.global_outages_v2`
+          WHERE datetime BETWEEN TIMESTAMP('{start_time_fmt}')
+                          AND TIMESTAMP('{end_time_fmt}')
+         """
 
     mozilla_df = None
     ne_mapping = pd.read_csv(NE_MAP_PATH)
@@ -44,35 +52,33 @@ def get_mozilla_data(country_name, end_time=datetime.datetime.now(), start_time=
     mozilla_with_ioda_id_df = mozilla_df.merge(ne_mapping,
                                                on=['country', 'geo_subdivision1', 'geo_subdivision2', 'city'])
 
-    assert len(mozilla_df) == len(mozilla_with_ioda_id_df), \
-        (f"Length mismatch: Original Mozilla DataFrame has {len(mozilla_df)} rows, "
-         f"DataFrame of Mozilla data merged with IODA ids has {len(mozilla_with_ioda_id_df)} rows")
+    # assert len(mozilla_df) == len(mozilla_with_ioda_id_df), \
+    #     (f"Length mismatch: Original Mozilla DataFrame has {len(mozilla_df)} rows, "
+    #      f"DataFrame of Mozilla data merged with IODA ids has {len(mozilla_with_ioda_id_df)} rows")
 
     region_agg_df = mozilla_with_ioda_id_df.groupby(["ioda_id", "datetime"]).agg({
-        "country": lambda country: list(set(country)),
+        "country": "first",
         "proportion_timeout": "mean",
         "proportion_unreachable": "mean",
         "city": lambda city: list(set(city))
     }).reset_index()
 
+    print(region_agg_df)
+
     print(f'Total no. of region-aggregated datapoints in country {country_name}: {len(region_agg_df)}')
     print(f'IODA regions present in data: {region_agg_df.ioda_id.unique()}')
 
-    country_agg_df = mozilla_with_ioda_id_df.groupby(["datetime"]).agg({
+    country_agg_df = mozilla_with_ioda_id_df.groupby(["datetime", "country"]).agg({
         "proportion_timeout": "mean",
         "proportion_unreachable": "mean",
         "city": lambda city: list(set(city)),
-        "geo_subdivision1": lambda subdiv1: list(set(subdiv1)),
-        "ioda_id": lambda id: list(set(id)),
-        "country": lambda country: list(set(country)),
     }).reset_index()
 
     # unpack lists and transform into string data for columns with list data
     # in addition, count number of cities in aggregated datapoint
-    region_df_string_cols = ['country', 'city']
-    country_df_string_cols = ['country', 'city', 'ioda_id', 'geo_subdivision1']
-    region_agg_df = transform_list_data_and_add_city_count(region_df_string_cols, region_agg_df)
-    country_agg_df = transform_list_data_and_add_city_count(country_df_string_cols, country_agg_df)
+    city_col_debugging = ['city']
+    region_agg_df = transform_list_data_and_add_city_count(city_col_debugging, region_agg_df)
+    country_agg_df = transform_list_data_and_add_city_count(city_col_debugging, country_agg_df)
 
     mozilla_with_ioda_id_df.to_csv('./data/merged.csv')
     region_agg_df.to_csv('./data/test_region.csv')
@@ -89,4 +95,4 @@ def transform_list_data_and_add_city_count(cols, df):
 
 
 if __name__ == "__main__":
-    get_mozilla_data()
+    get_mozilla_data('US')
